a671553439de685643a1c5ccdca258857083581c
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-01-11 22:57:39 +0300

    pack text as indexed array

--- generate-text.py
+++ generate-text.py
@@ -8,7 +8,17 @@ parser.add_argument('source', help='input file')
 args = parser.parse_args()
 
 messages = json.load(open(args.source))
+offsets = []
+data = []
 for key, value in messages.iteritems():
-	print ": text_%s\n\t" %key,
-	print " ".join(["0x%02x" %ord(i) for i in value]),
-	print "0\n"
+	offsets.append(len(data))
+	print ": const text_%s %d\n" %(key, len(offsets)),
+	for ch in value:
+		data.append(ord(ch))
+	data.append(0)
+
+print ": data_text\n\t",
+print " ".join(["0x%02x" %i for i in data])
+
+print ": data_text_index\n\t" + " ".join(["0x%02x 0x%02x" %(x & 0xff, x >> 8) for x in offsets])
+print
