8c8e65a12ad9aa2b45f9025affed108b9a602cc1
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2016-01-14 00:15:40 +0300

    added pdm-based encoder for audio

---
+++ generate-audio-pdm.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import argparse
+import wave
+import struct
+
+from fractions import gcd
+
+parser = argparse.ArgumentParser(description='Convert audio.')
+parser.add_argument('source', help='input file')
+parser.add_argument('name', help='name')
+args = parser.parse_args()
+
+wav = wave.open(args.source)
+n = wav.getnframes()
+if wav.getsampwidth() != 2:
+	raise Exception("invalid sample width")
+if wav.getnchannels() != 1:
+	raise Exception("only mono supported")
+if wav.getframerate() != 4000:
+	raise Exception("invalid sample rate, sox -S %s -r 4000 -b 16 <output>" %args.source)
+
+frames = wav.readframes(n)
+
+offset = 0
+size = 0
+bit, byte = 0, 0
+
+source = ""
+source += ": audio_%s\n" %args.name
+
+for i in xrange(0, n):
+	buf = []
+
+	value, = struct.unpack('<h', frames[offset: offset + 2]) if offset < len(frames) else (0,)
+	offset += 2
+
+	x = value / 32768.0
+	assert x >= -1 and x <= 1
+	qe = 0
+
+	if x >= qe:
+		byte |= (0x80 >> bit)
+		y = 1
+	else:
+		y = -1
+	qe = y - x + qe
+
+	bit += 1
+	if bit == 8:
+		source += "0x%02x " %byte
+		bit = 0
+		byte = 0
+		size += 1
+		if (size % 32) == 0:
+			source += "\n"
+
+if size % 16:
+	rem = 16 - (size % 16)
+	for i in xrange(0, rem):
+		source += "0x00 "
+		size += 1
+
+print
+size /= 16 #loop count
+#print ":const audio_%s_size %d"  %(args.name, size)
+print ": audio_%s_size\n\t0x%02x 0x%02x\n%s"  %(args.name, size & 0xff, size >> 8, source)
